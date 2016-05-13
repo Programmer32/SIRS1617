6dd102d10a05be0886d3a09c86e0e8c1ae51ee72
Miguel Amaral <miguel.amaral04@gmail.com>
2016-05-13 13:58:51 +0100

    keys deleted

--- CA-ws/cleanKeys.sh
+++ CA-ws/cleanKeys.sh
@@ -1,1 +1,12 @@
-rm -v src/main/resources/*.key
+rm -rv keys_*
+cd ..
+
+for folder in */
+do
+	cd $folder
+	for extension in .pem.txt .cer .jks .key
+	do
+		rm -rv ./src/main/resources/*$extension
+	done
+	cd ..
+done
